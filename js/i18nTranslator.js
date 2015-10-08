68348ab71078f51a5fb27ac1a435e1a38d9e995f
Gong Yu <hollomyfoolish@163.com>
2015-10-08 17:45:16 +0800

    create project

---
+++ js/i18nTranslator.js
@@ -0,0 +1,149 @@
+/**
+ * New node file
+ */
+
+var DOMParser = require('xmldom').DOMParser,
+    XMLSerializer = require('xmldom').XMLSerializer,
+    fs = require('fs'),
+    slice = Array.prototype.slice,
+    XML_PATTERN = /\.xml$/i,
+    JS_PATTERN = /\.js$/i,
+    CONTENT_NAME = 'BUSMB_SBO_B1A_DemandPlanning_UI_1_1_92DEV',
+    jade = require('jade'),
+    path = require('path'),
+    console = window?window.console:global.console;
+
+function Translator(srcDir, destDir, contentName, collectionName){
+    this.srcDir = srcDir;
+    this.destDir = destDir;
+    this.contentName = contentName;
+    this.collectionName = collectionName;
+}
+
+Translator.prototype = {
+    translate: function(){
+        this.processDir();
+    },
+
+    processDir: function(){
+        var srcDir = this.srcDir,
+            destDir = this.destDir,
+            translateFile = this.translateFile.bind(this),
+            writeDestFile = this.writeDestFile.bind(this),
+            needProcess = this.needProcess.bind(this);
+        fs.readdir(srcDir, function(err, files){
+            if(err){
+                console.log(err);
+                return;
+            }
+            files.forEach(function(fName, idx){
+                var filePath = srcDir + '/' + fName;
+                fs.stat(filePath, function(err, fileStat){
+                    if(err){
+                        console.log(err);
+                        return;
+                    }
+                    if(fileStat.isFile() && needProcess(fName)){
+                        translateFile(filePath, fName, destDir);
+                    }
+                });
+            });
+        });
+    },
+
+    translateFile: function(filePath, srcFileName, destDir){
+        throw 'sub-class should inplement this method';
+    },
+
+    writeDestFile: function(fileName, content){
+        fs.writeFile(this.destDir + '/' + fileName, content, {encoding: 'utf8'}, function (err) {
+            if (err) {
+                console.log(fileName + ' error: ' + err);
+            } else {
+                console.log(fileName +': done');
+            }
+        });
+    },
+
+    needProcess: function(fileName){
+        return true;
+    }
+};
+
+function Xml2JsTranslator(srcDir, destDir){
+    Translator.apply(this, arguments);
+}
+
+Xml2JsTranslator.prototype = Object.create(Translator.prototype, {
+    translateFile: {
+        value: function(filePath, srcFileName, destDir){
+            var me = this;
+            fs.readFile(filePath, {encoding: 'utf8'}, function (err, data) {
+                var nameParts = srcFileName.split('_'),
+                    jsFileName = nameParts[nameParts.length - 1].replace(XML_PATTERN, '.js');
+                    i18nStr = ['/*\r\n',
+                               ' * SAP Business One Demand Planning UI Localized Resources for ' + jsFileName.substring(0, jsFileName.length-3) + '\r\n',
+                               ' */\r\n',
+                               'var i18n = {\r\n'],
+                    indent = '    ';
+                var dom = new DOMParser().parseFromString(data);
+                var loEls = dom.getElementsByTagName('lo');
+                slice.call(loEls, 0).forEach(function(loEl, idx){
+                    var key = loEl.getAttribute('tK'),
+                        value = loEl.getElementsByTagName('loC')[0].firstChild.data.trim();
+                    i18nStr.push(indent);
+                    i18nStr.push('"' + key + '": "' + value + '"');
+                    i18nStr.push(',\r\n');
+                });
+                i18nStr[i18nStr.length - 1] = '\r\n};';
+                me.writeDestFile(jsFileName, i18nStr.join(''));
+            });
+        }
+    },
+
+    needProcess: {
+        value: function(fileName){
+            return XML_PATTERN.test(fileName);
+        }
+    }
+});
+
+function Js2XmlTranslator(srcDir, destDir, contentName, collectionName){
+    Translator.apply(this, arguments);
+    this.generator = jade.compile(fs.readFileSync(path.join(__dirname, '../resource/xml-template.xml')),
+     {
+        doctype: 'xml',
+        pretty: true
+    });
+}
+
+Js2XmlTranslator.prototype = Object.create(Translator.prototype, {
+    translateFile: {
+        value: function(filePath, srcFileName, destDir){
+            var me = this,
+                language = srcFileName.replace(/\.js$/i, '');
+            fs.readFile(filePath, {encoding: 'utf8'}, function (err, data) {
+                eval(data);
+                var xml = me.generator({
+                    language: language,
+                    collectionName: me.collectionName,
+                    domain: 'FI',
+                    contentName: me.contentName,
+                    i18n: i18n
+                });
+                me.writeDestFile(me.contentName + '_' + language + '.xml', xml);
+            });
+        }
+    },
+
+    needProcess: {
+        value: function(fileName){
+            return JS_PATTERN.test(fileName);
+        }
+    }   
+});
+
+module.exports = {
+    Xml2JsTranslator: Xml2JsTranslator,
+    Js2XmlTranslator: Js2XmlTranslator
+};
---
+++ js/main.js
@@ -0,0 +1,80 @@
+(function(){
+    var messagePane = $('#message');
+    console = {
+        log: function(s){
+            var html = '<h3>' + new Date().toLocaleTimeString() + '</h3>'
+                + '<li>' + s + '</li>';
+            messagePane.prepend(html);
+        },
+
+        error: function(s){
+            this.log(s);
+        }
+    };
+    var slice = Array.prototype.slice;
+    var isBlank = function(s){
+        return !s || s.trim().length === 0;
+    };
+
+    var fileUtil = {
+        readFiles: function(files){
+            slice.call(files, 0).forEach(function(f){
+                var fr = new FileReader(f);
+                fr.onload = function(e){
+                    var data = e.currentTarget.result;
+                };
+            });
+        }
+    };
+
+    var main = $('#main');
+    main.find('.not-allow-blank').on('input', function(e){
+        $(this).removeClass('not-allow-blank');
+    });
+    main.find('input').blur(function(){
+        var input = $(this);
+        if(isBlank(input.val()) && !input.parent('.field').hasClass('not-allow-blank')){
+            input.parent('.field').addClass('not-allow-blank');
+        }
+    });
+    main.find('input[name="translationType"]').change(function(event) {
+        if($(this).val() === 'XmlToJs'){
+            $('.jsToXml').hide();
+        }else{
+            $('.jsToXml').show();
+        }
+    });
+
+    // var srcFilesDrop = document.querySelector('#srcFiles');
+    // srcFilesDrop.addEventListener('dragover', function(e){
+    //     e.stopPropagation();
+    //     e.preventDefault();
+    //     e.dataTransfer.dropEffect = 'copy';
+    // });
+    // srcFilesDrop.addEventListener('drop', function(e){
+    //     e.stopPropagation();
+    //     e.preventDefault();
+    //     fileUtil.readFiles(e.dataTransfer.files);
+    // });
+
+    $('#submit').click(function(event) {
+        event.stopPropagation();
+        event.preventDefault();
+        var srcDir = $('#srcFolder').val(),
+            destDir = $('#destFolder').val(),
+            contentName = $('#contentName').val(),
+            collectionName = $('#collectionName').val(),
+            translatorType = $('input[name="translationType"]:checked').val(),
+            translator = require('./js/i18nTranslator.js');
+
+        switch(translatorType){
+            case 'XmlToJs':
+                return new translator.Xml2JsTranslator(srcDir, destDir, contentName).translate();
+            case 'JsToXml':
+                return new translator.Js2XmlTranslator(srcDir, destDir, contentName, collectionName).translate();
+            default:
+                return;
+        }
+    });
+
+})();
